2b5fd121dad38ab04dedbe83a406d85bf2dcefcf
erotourtes <erotourtes@gmail.com>
2024-04-05 14:02:39 +0300

    Refactor(mocks): extracted into separate folders

--- frontend/tests/components/TaskList/ListColumn.test.tsx
+++ frontend/tests/components/TaskList/ListColumn.test.tsx
@@ -1,61 +1,20 @@
-import { TaskListT, TaskPriority, TaskT } from "@packages/types";
+import { TaskListT, TaskT } from "@packages/types";
 import { render } from "@testing-library/react";
 import { ListColumn } from "@components/TaskList/TaskList.tsx";
-import fetchMock from "jest-fetch-mock";
-import { MockHooksT } from "../../test-utils.ts";
 import { useGetTasksForListQuery } from "@redux/api/hooks.ts";
+import { mockTaskLists, mockTasks } from "../../__mocks__/typeMocks.ts";
 
 jest.mock("@/redux/api/wsService.ts");
-jest.mock("@/redux/api/errorMiddleware.ts")
+jest.mock("@/redux/api/hooks.ts");
 
-// TODO: can't setup manual mock for useGetTasksForListQuery
-jest.mock("@/redux/api/hooks.ts", (): MockHooksT => ({
-  useDeleteNewListMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useUpdateNewListMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useGetTasksForListQuery: jest.fn(() => ({ data: [] })),
-  useDeleteTaskMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useUpdateTaskMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useGetAllTaskListsQuery: jest.fn(() => ({ data: [] })),
-  useCreateNewListMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useCreateNewTaskMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useCreateBoardMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useDeleteBoardMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useUpdateBoardMutation: jest.fn(() => [jest.fn(), { isLoading: false }]),
-  useGetAllBoardsQuery: jest.fn(() => ({ data: [] })),
-  useGetAllHistoryQuery: jest.fn(() => ({ data: [] })),
-  useGetHistoryForTaskQuery: jest.fn(() => ({ data: [] })),
-}));
-
-let list: TaskListT
-let tasks: TaskT[]
 describe("ListColumn", () => {
+  let list: TaskListT
+  let tasks: TaskT[]
+
   beforeEach(() => {
-    fetchMock.resetMocks();
-    list = {
-      id: 1,
-      name: "List 1",
-      boardId: 1,
-    }
-    tasks = [
-      {
-        id: 1,
-        name: "Task 1",
-        list: { id: 1 },
-        description: "Description 1",
-        dueDate: "2022-01-01",
-        priority: TaskPriority.LOW
-      },
-      {
-        id: 2,
-        name: "Task 2",
-        list: { id: 1 },
-        description: "Description 2",
-        dueDate: "2022-01-01",
-        priority: TaskPriority.MEDIUM
-      }
-    ]
-  })
-  ;
+    list = mockTaskLists()[0]
+    tasks = mockTasks()
+  });
 
   it("should have skeleton", () => {
     list.id = -1;
--- frontend/tests/test-utils.ts
+++ frontend/tests/test-utils.ts
@@ -1,7 +1,4 @@
 import * as hooks from '@/redux/api/hooks';
 
 export type HooksT = keyof typeof hooks;
-/* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-export type PartialMockHooksT = Partial<Record<HooksT, any>>;
-/* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-export type MockHooksT = Record<HooksT, any>;
+export type MockHooksT = Record<HooksT, jest.Mock>;
