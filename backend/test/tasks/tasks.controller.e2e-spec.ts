6635ba0e50a409f2ccbaa567b33facfc2968145e
erotourtes <erotourtes@gmail.com>
2024-04-06 17:23:48 +0300

    Refactor: extracted stabs to a separate folder

--- backend/test/tasks/tasks.controller.e2e-spec.ts
+++ backend/test/tasks/tasks.controller.e2e-spec.ts
@@ -2,55 +2,12 @@ import { WsAdapter } from '@nestjs/platform-ws';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from 'src/app.module';
-import { Task } from 'src/tasks/tasks.entity';
 import { TaskPriority } from '@packages/types';
 import { DataSource } from 'typeorm';
 import { INestApplication } from '@nestjs/common';
-import { TaskList } from 'src/task-lists/task-lists.entity';
 import { Board } from 'src/board/board.entity';
 import { TaskHistoryDbSubscriber } from 'src/history/task/tasks.dbsubscriber';
-
-const boardStab = (): Partial<Board>[] => [{ name: 'Board 1' }];
-const listStab = (): Partial<TaskList>[] => [
-  { name: 'Task List 1' },
-  { name: 'Task List 2' },
-];
-
-const taskStab = (): Partial<Task>[] => [
-  {
-    name: 'Task 1',
-    description: 'Description 1',
-    priority: TaskPriority.LOW,
-    dueDate: new Date('2014-02-18').toISOString(),
-  },
-  {
-    name: 'Task 2',
-    description: 'Description 2',
-    priority: TaskPriority.MEDIUM,
-  },
-];
-
-const createTasks = async (dbSource: DataSource) => {
-  const boards = await dbSource.manager.save(Board, boardStab());
-  const lists = await dbSource.manager.save(
-    TaskList,
-    listStab().map((taskList) => ({ ...taskList, board: boards[0] })),
-  );
-  const tasks0 = await dbSource.manager.save(
-    Task,
-    taskStab().map((task) => ({ ...task, list: lists[0] })),
-  );
-  const tasks1 = await dbSource.manager.save(
-    Task,
-    taskStab().map((task) => ({ ...task, list: lists[1] })),
-  );
-  return {
-    boards,
-    lists,
-    tasks0,
-    tasks1,
-  };
-};
+import { createTasks } from 'test/stabs/stabs';
 
 describe('TaskController', () => {
   let app: INestApplication<any>;
