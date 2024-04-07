daae1f942680fb9a42b954923cc38dc85841ff4f
erotourtes <erotourtes@gmail.com>
2024-04-07 23:38:19 +0300

    Fix(history): using same entity manager to avoid race conditions
    Couldn't craete history in some rare cases becase of how typeorm works_

--- backend/src/history/history.module.ts
+++ backend/src/history/history.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { History } from './history.entity';
 import { TaskHistoryDbSubscriber } from './task/tasks.dbsubscriber';
-import { TaskHistoryService } from './task/tasks.service';
+import {
+  TaskHistoryCreatorService,
+  TaskHistoryService,
+} from './task/tasks.service';
 import { TaskHistoryController } from './task/task.controller';
 import { TaskHistoryGateway } from './task/task.gateway';
 import { TaskList } from 'src/task-lists/task-lists.entity';
@@ -12,7 +15,12 @@ import { TaskList } from 'src/task-lists/task-lists.entity';
     TypeOrmModule.forFeature([History]),
     TypeOrmModule.forFeature([TaskList]),
   ],
-  providers: [TaskHistoryService, TaskHistoryDbSubscriber, TaskHistoryGateway],
+  providers: [
+    TaskHistoryService,
+    TaskHistoryDbSubscriber,
+    TaskHistoryGateway,
+    TaskHistoryCreatorService,
+  ],
   controllers: [TaskHistoryController],
   exports: [],
 })
--- backend/src/history/task/tasks.service.ts
+++ backend/src/history/task/tasks.service.ts
@@ -1,6 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from 'src/tasks/tasks.entity';
-import { Repository } from 'typeorm';
+import { Repository, EntityManager } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { HistoryActionType, History } from '../history.entity';
@@ -9,35 +9,34 @@ import { HistoryT } from '@packages/types';
 import { TaskList } from 'src/task-lists/task-lists.entity';
 
 @Injectable()
-export class TaskHistoryService {
+export class TaskHistoryCreatorService {
   logger: Logger;
-  constructor(
-    @InjectRepository(History)
-    private readonly historyRepository: Repository<History>,
-    private readonly taskGateway: TaskHistoryGateway,
-    @InjectRepository(TaskList)
-    private readonly taskListRepository: Repository<TaskList>,
-  ) {
-    this.logger = new Logger(`${TaskHistoryGateway.name}`);
+  constructor(private readonly taskGateway: TaskHistoryGateway) {
+    this.logger = new Logger(`${TaskHistoryCreatorService.name}`);
   }
 
-  async create(record: {
-    actionType: HistoryActionType;
-    taskName: string;
-    fieldName?: keyof Task;
-    oldValue?: string;
-    newValue?: string;
-    recordId: number;
-    boardId: number;
-  }) {
-    const newRecord = this.historyRepository.create({
+  async create(
+    record: {
+      actionType: HistoryActionType;
+      taskName: string;
+      fieldName?: keyof Task;
+      oldValue?: string;
+      newValue?: string;
+      recordId: number;
+      boardId: number;
+    },
+    entityManager: EntityManager,
+  ) {
+    // Can't inject repositories due to https://github.com/typeorm/typeorm/issues/3563#issuecomment-460054971
+    const historyRepository = entityManager.getRepository(History);
+    const newRecord = historyRepository.create({
       ...record,
       oldValue: record.oldValue?.substring(0, 128),
       newValue: record.newValue?.substring(0, 128),
       fieldName: record.fieldName as string,
     });
 
-    const history = await this.historyRepository.save(newRecord).catch((e) => {
+    const history = await historyRepository.save(newRecord).catch((e) => {
       this.logger.error({
         status: 'History record creation failed',
         record,
@@ -47,7 +46,11 @@ export class TaskHistoryService {
     });
 
     // Mutates the history object
-    if (record.fieldName === 'list') await this.joinSingleListName(history);
+    if (record.fieldName === 'list')
+      await this.joinSingleListName(
+        entityManager.getRepository(TaskList),
+        history,
+      );
 
     this.taskGateway.sendHistoryUpdate({
       ...history,
@@ -58,6 +61,42 @@ export class TaskHistoryService {
       status: 'History record created',
       record,
     });
+  }
+
+  // TODO: refactor
+  private async joinSingleListName(
+    taskListRepository: Repository<TaskList>,
+    history: HistoryT,
+  ) {
+    if (history.fieldName !== 'list') return;
+
+    const oldList = await taskListRepository.findOne({
+      select: { name: true },
+      where: { id: +history.oldValue },
+    });
+
+    const newList = await taskListRepository.findOne({
+      select: { name: true },
+      where: { id: +history.newValue },
+    });
+
+    history.data = {
+      oldListName: oldList?.name,
+      newListName: newList?.name,
+    };
+  }
+}
+
+@Injectable()
+export class TaskHistoryService {
+  logger: Logger;
+  constructor(
+    @InjectRepository(History)
+    private readonly historyRepository: Repository<History>,
+    @InjectRepository(TaskList)
+    private readonly taskListRepository: Repository<TaskList>,
+  ) {
+    this.logger = new Logger(`${TaskHistoryGateway.name}`);
   }
 
   async findAll(boardId: number): Promise<History[]> {
